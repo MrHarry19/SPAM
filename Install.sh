c4902e5f2ad7ece835981263d27f8103342fedca
MrHarry19 <50568113+MrHarry19@users.noreply.github.com>
2019-05-13 14:46:42 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,4 +1,11 @@
-
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
 
 clear
 echo
@@ -79,4 +86,4 @@ sleep 2
 echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
 exit
 fi
-fi
+
